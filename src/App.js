550713a4e6d876eb5bc40648c5bfbea56f093517
Brendon Pagano <brendonpagano@gmail.com>
2018-08-27 04:18:57 -0300

    Mounting initial components structure

--- src/App.js
+++ src/App.js
@@ -6,20 +6,45 @@ import io from 'socket.io-client';
 
 
 // Custom Components
+import MessageContainer from './components/MessageContainer';
+import Message from './components/Message';
 
 
 
 class App extends Component {
 
+    state = {
+        messages: [
+            { user: 'Annatsu', content: 'Hello guys!' },
+            { user: 'Qu4dr4tus', content: 'Hi, howza goin?' },
+            { user: 'Hipnode', content: 'typing...' }
+        ]
+    }
+
+
     componentDidMount() {
         // Creates an Socket.io instance.
         const socket = io();
+
+
+        // Setups a socket listener for new messages
+        socket.on('newMessage', (msg) => {
+            alert('msg' + msg)
+        });
     }
 
 
     render() {
+        const { messages } = this.state;
+
         return (
-            <div>My App</div>
+            <div>
+                {messages.map((msg, i) => (
+                    <MessageContainer key={i} username={msg.user}>
+                        <Message>{msg.content}</Message>
+                    </MessageContainer>
+                ))}
+            </div>
         );
     }
 
